bb06334bc5bad7a730e22c9ae514cb1d5bc055f1
MisaelGC <gacrmu@hotmail.com>
2021-01-11 17:15:54 -0600

    Mi Blog personal

---
+++ assets/js/dist/index.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// scrollHeader
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+  var menuToogle = document.querySelector(".toggle");
+  var menu = document.querySelector(".menu");
+  menuToogle.classList.toggle("active");
+  menu.classList.toggle("active");
+}
